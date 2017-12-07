15ecde883d1a24d7ce16ad61682ad5d671cd9489
Benjamin Gandon <benjamin@gstack.io>
2017-12-07 18:53:17 +0100

    Start a SHIELD v8 deployment

--- deployments/_cloud-config/conf/operations-layout.inc.bash
+++ deployments/_cloud-config/conf/operations-layout.inc.bash
@@ -3,4 +3,5 @@ upstream_operations=(
 local_operations=(
     cf-mysql-support
     concourse-support
+    shield-support
 )
---
+++ deployments/shield/conf/operations-layout.inc.bash
@@ -0,0 +1,7 @@
+local_operations=(
+	inject-variables
+    set-versions
+    routing
+)
+upstream_operations=(
+)
