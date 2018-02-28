bf32525e8276f731f16f8495844f40d7adfdcd50
Benjamin Gandon <benjamin@gstack.io>
2018-03-01 00:54:52 +0100

    Run cassandra smoke tests at the end of each 'gbe converge'

--- deployments/cassandra/post-deploy-once.sh
+++ deployments/cassandra/post-deploy-once.sh
@@ -1,50 +1,5 @@
 #!/usr/bin/env bash
 
-function cf_state_var() {
-    local state_file=$1
-    local path=$2
-    bosh int "$BASE_DIR/state/cf/$state_file.yml" --path "$path"
-}
-
-function cf_depl_var() {
-    cf_state_var depl-manifest "$1"
-}
-
-function cf_creds_var() {
-    cf_state_var depl-creds "$1"
-}
-
-function cf_login() {
-    cf_api_url=$(cf_depl_var /instance_groups/name=api/jobs/name=cf-admin-user/properties/api_url)
-    cf_skip_ssl_validation=$(cf_depl_var /instance_groups/name=smoke-tests/jobs/name=smoke_tests/properties/smoke_tests/skip_ssl_validation)
-    cf api "$cf_api_url" ${cf_skip_ssl_validation:+--skip-ssl-validation}
-
-    cf_admin_username=$(cf_depl_var /instance_groups/name=api/jobs/name=cf-admin-user/properties/admin_username)
-    set +x
-    cf_admin_password=$(cf_creds_var /cf_admin_password)
-    echo "cf auth '$cf_admin_username' '<redacted>'"
-    cf auth "$cf_admin_username" "$cf_admin_password"
-    set -x
-}
-
 set -ex
 
 bosh run-errand broker-registrar
-
-
-cf_login
-
-cf create-org service-sandbox
-cf create-space cassandra-smoke-tests -o service-sandbox
-
-function cleanup() {
-    cf target -o service-sandbox -s cassandra-smoke-tests
-    cf delete cassandra-example-app -r -f
-    cf delete-space cassandra-smoke-tests -o service-sandbox -f
-    cf delete-org service-sandbox -f
-    cf logout
-}
-
-trap cleanup EXIT
-
-bosh run-errand cassandra-smoke-tests
