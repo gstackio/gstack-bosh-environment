018ae60f5fab1ff2d6758cd23d30492c74edef23
Benjamin Gandon <benjamin@gstack.io>
2018-03-01 00:23:50 +0100

    Bump cassandra to v6

--- deployments/cassandra/pre-deploy-once.sh
+++ deployments/cassandra/pre-deploy-once.sh
@@ -32,7 +32,7 @@ set -ex
 pushd "$BASE_DIR/.cache/resources/cassandra-boshrelease" || exit 115
     git submodule update --init --recursive
     bosh reset-release
-    bosh create-release
+    bosh create-release --force
     bosh upload-release
 popd
 
