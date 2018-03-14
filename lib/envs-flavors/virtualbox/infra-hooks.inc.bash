880a62ad3e0389fee911c1990dadd76294ab871d
Benjamin Gandon <benjamin@gstack.io>
2018-03-14 11:26:30 +0100

    Bump Prometheus to v21.1.1 + Fix import of 'internal_ip' bbl var in the 'ddbox-env'

--- lib/envs-flavors/virtualbox/infra-hooks.inc.bash
+++ lib/envs-flavors/virtualbox/infra-hooks.inc.bash
@@ -22,7 +22,8 @@ function pre_create_env_hook() {
 }
 
 function extern_infra_vars_hook() {
-    echo '--- {}'
+    local internal_ip=$(spec_var /deployment_vars/internal_ip "$BASE_DIR/$GBE_ENVIRONMENT")
+    echo "--- { internal_ip: \"$internal_ip\" }"
 }
 
 function post_create_env_hook() {
@@ -46,6 +47,25 @@ function setup_firewall_hook() {
 
     if [ -z "$nic_num" ]; then
         # Nothing to do when no NIC is attached to any NAT connection
+
+        # FIXME: we now have some kludge shim here
+        local external_ip=$(external_ip)
+        local traefik_ip=10.244.0.34
+        local default_network=10.244.0.0/20
+        if ! gbe ssh sudo iptables -t nat -nvL | grep -qF gbe[f55fc128-6cec-4f38-9303-2e45b0010c76]; then
+            gbe ssh sudo iptables -t nat -I PREROUTING 2 -i w+ -p tcp -d "$external_ip" --dport 80 -j DNAT --to-dest "$traefik_ip" -m comment --comment "gbe[f55fc128-6cec-4f38-9303-2e45b0010c76]"
+        fi
+        if ! gbe ssh sudo iptables -t nat -nvL | grep -qF gbe[f55fc128-6cec-4f38-9303-2e45b0010c77]; then
+            gbe ssh sudo iptables -t nat -I PREROUTING 3 -i w+ -p tcp -d "$external_ip" --dport 443 -j DNAT --to-dest "$traefik_ip" -m comment --comment "gbe[f55fc128-6cec-4f38-9303-2e45b0010c77]"
+
+        fi
+        if ! gbe ssh sudo iptables -t nat -nvL | grep -qF gbe[f55fc128-6cec-4f38-9303-2e45b0010c78]; then
+            gbe ssh sudo iptables -t nat -I POSTROUTING 1 -o w+ -p tcp -s "$default_network" -d "$traefik_ip" --dport 80 -j MASQUERADE -m comment --comment "gbe[f55fc128-6cec-4f38-9303-2e45b0010c78]"
+        fi
+        if ! gbe ssh sudo iptables -t nat -nvL | grep -qF gbe[f55fc128-6cec-4f38-9303-2e45b0010c79]; then
+            gbe ssh sudo iptables -t nat -I POSTROUTING 2 -o w+ -p tcp -s "$default_network" -d "$traefik_ip" --dport 443 -j MASQUERADE -m comment --comment "gbe[f55fc128-6cec-4f38-9303-2e45b0010c79]"
+        fi
+
         return
     fi
 
