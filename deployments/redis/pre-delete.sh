e4fd4c060618976fceee614cd660b92d5588add4
Benjamin Gandon <benjamin@gstack.io>
2018-06-19 21:28:52 +0200

    Add a new subsystem to deploy Redis v431.0.0 along with its service broker

---
+++ deployments/redis/pre-delete.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SUBSYS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+function spec_var() {
+    local path=$1
+    bosh int "$SUBSYS_DIR/conf/spec.yml" --path "$path"
+}
+
+set -ex
+
+if bosh deployments | grep -qE "\\b$(spec_var /deployment_vars/deployment_name)\\b"; then
+    bosh run-errand broker-deregistrar
+fi
