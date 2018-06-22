da7c90920772f9133bbd61c7dca562d0ae0fb040
Benjamin Gandon <benjamin@gstack.io>
2018-06-22 21:43:04 +0200

    Bump to Cassandra release v8

--- deployments/cassandra/pre-deploy-once.sh
+++ deployments/cassandra/pre-deploy-once.sh
@@ -36,19 +36,27 @@ function cf_login() {
 
 set -ex
 
+create_release=false
 dev_release_name=cassandra
 
-rsc_name=$(spec_var /input_resources/0/name)
-pushd "$BASE_DIR/.cache/resources/$rsc_name" || exit 115
-    git submodule update --init --recursive
+if [[ $create_release == true ]]; then
+    rsc_name=$(spec_var /input_resources/0/name)
+    pushd "$BASE_DIR/.cache/resources/$rsc_name" || exit 115
+        git submodule update --init --recursive
 
-    latest_dev_release=$(ls -t dev_releases/$dev_release_name/$dev_release_name-*.yml 2> /dev/null | head -n 1)
-    if [ -z "$latest_dev_release" -o "$(bosh int "$latest_dev_release" --path /commit_hash)" != "$(git rev-parse --short HEAD)" ]; then
-        bosh reset-release
-        bosh create-release --force
-    fi
-    bosh upload-release
-popd
+        latest_dev_release=$(ls -t dev_releases/$dev_release_name/$dev_release_name-*.yml 2> /dev/null | head -n 1)
+        if [[ -z $latest_dev_release \
+                || $(bosh int "$latest_dev_release" --path /commit_hash) != $(git rev-parse --short HEAD) ]]; then
+            bosh reset-release
+            bosh create-release --force
+        fi
+        # base_version=8
+        # if bosh inspect-release "$dev_release_name/${base_version}+dev.1" &> /dev/null; then
+        #     bosh delete-release "$dev_release_name/${base_version}+dev.1"
+        # fi
+        bosh upload-release
+    popd
+fi
 
 cf_login
 
