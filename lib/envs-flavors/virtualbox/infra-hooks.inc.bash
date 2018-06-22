446131cd8215af6aabe15ec814992625c1368854
Benjamin Gandon <benjamin@gstack.io>
2018-06-22 23:26:07 +0200

    Experiment using network-release in order to transfer the VM networking fixup iptable rules to the bosh create-env deployment

--- lib/envs-flavors/virtualbox/infra-hooks.inc.bash
+++ lib/envs-flavors/virtualbox/infra-hooks.inc.bash
@@ -57,21 +57,34 @@ function setup_firewall_hook() {
         # Nothing to do when no NIC is attached to any NAT connection
 
         # FIXME: we now have some kludge shim here
-        local external_ip=$(external_ip)
-        local traefik_ip=10.244.0.34
-        local default_network=10.244.0.0/20
+        local external_ip traefik_ip default_network
+        external_ip=$(external_ip)
+        traefik_ip=10.244.0.34 # 10.244.0.143
+        default_network=10.244.0.0/20
         if ! ssh_jumpbox sudo iptables -t nat -nvL | grep -qF gbe[f55fc128-6cec-4f38-9303-2e45b0010c76]; then
-            ssh_jumpbox sudo iptables -t nat -I PREROUTING 2 -i w+ -p tcp -d "$external_ip" --dport 80 -j DNAT --to-dest "$traefik_ip" -m comment --comment "gbe[f55fc128-6cec-4f38-9303-2e45b0010c76]"
+            ssh_jumpbox sudo iptables -t nat -I PREROUTING 2 \
+                -i w+ -p tcp -d "$external_ip" --dport 80 \
+                -j DNAT --to-dest "$traefik_ip" \
+                -m comment --comment "gbe[f55fc128-6cec-4f38-9303-2e45b0010c76]"
         fi
         if ! ssh_jumpbox sudo iptables -t nat -nvL | grep -qF gbe[f55fc128-6cec-4f38-9303-2e45b0010c77]; then
-            ssh_jumpbox sudo iptables -t nat -I PREROUTING 3 -i w+ -p tcp -d "$external_ip" --dport 443 -j DNAT --to-dest "$traefik_ip" -m comment --comment "gbe[f55fc128-6cec-4f38-9303-2e45b0010c77]"
+            ssh_jumpbox sudo iptables -t nat -I PREROUTING 3 \
+                -i w+ -p tcp -d "$external_ip" --dport 443 \
+                -j DNAT --to-dest "$traefik_ip" \
+                -m comment --comment "gbe[f55fc128-6cec-4f38-9303-2e45b0010c77]"
 
         fi
         if ! ssh_jumpbox sudo iptables -t nat -nvL | grep -qF gbe[f55fc128-6cec-4f38-9303-2e45b0010c78]; then
-            ssh_jumpbox sudo iptables -t nat -I POSTROUTING 1 -o w+ -p tcp -s "$default_network" -d "$traefik_ip" --dport 80 -j MASQUERADE -m comment --comment "gbe[f55fc128-6cec-4f38-9303-2e45b0010c78]"
+            ssh_jumpbox sudo iptables -t nat -I POSTROUTING 1 \
+                -o w+ -p tcp -s "$default_network" -d "$traefik_ip" --dport 80 \
+                -j MASQUERADE \
+                -m comment --comment "gbe[f55fc128-6cec-4f38-9303-2e45b0010c78]"
         fi
         if ! ssh_jumpbox sudo iptables -t nat -nvL | grep -qF gbe[f55fc128-6cec-4f38-9303-2e45b0010c79]; then
-            ssh_jumpbox sudo iptables -t nat -I POSTROUTING 2 -o w+ -p tcp -s "$default_network" -d "$traefik_ip" --dport 443 -j MASQUERADE -m comment --comment "gbe[f55fc128-6cec-4f38-9303-2e45b0010c79]"
+            ssh_jumpbox sudo iptables -t nat -I POSTROUTING 2 \
+                -o w+ -p tcp -s "$default_network" -d "$traefik_ip" --dport 443 \
+                -j MASQUERADE \
+                -m comment --comment "gbe[f55fc128-6cec-4f38-9303-2e45b0010c79]"
         fi
 
         return
