b127c6fe3d29a4d1f5619c8870e03eb6c08ff28d
Benjamin Gandon <benjamin@gstack.io>
2018-07-04 06:13:54 +0200

    Fix the 'post-deploy' hook for compiled-release helper

--- deployments/zzz-compiled-release-helper/post-deploy.sh
+++ deployments/zzz-compiled-release-helper/post-deploy.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 SUBSYS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+BASE_DIR=$(cd "$SUBSYS_DIR/../.." && pwd)
 
 function runtime_config_var() {
     local path=$1
-    bosh int "$GBE_ENVIRONMENT/runtime-config/conf/spec.yml" --path "/config_vars$path"
+    bosh int "$BASE_DIR/$GBE_ENVIRONMENT/runtime-config/conf/spec.yml" --path "/config_vars${path}"
 }
 
 function spec_var() {
@@ -12,13 +15,18 @@ function spec_var() {
     bosh int "$SUBSYS_DIR/conf/spec.yml" --path "$path"
 }
 
+function cf_depl_var() {
+    local path=$1
+    bosh int "$SUBSYS_DIR/../cf/conf/spec.yml" --path "/deployment_vars${path}"
+}
+
 deployment_name=$(spec_var /deployment_vars/deployment_name)
 bosh_dns_version=$(runtime_config_var /bosh_dns_version)
 node_exporter_version=$(runtime_config_var /node_exporter_version)
-stemcell_version=$(spec_var /deployment_vars/stemcell_version)
+stemcell_version=$(cf_depl_var /stemcell_version)
 subsys_name=$(spec_var /subsys/name)
 
-set -e
+set -x
 
 bosh -d "$deployment_name" \
     export-release \
@@ -31,4 +39,4 @@ bosh -d "$deployment_name" \
     --dir="$BASE_DIR/.cache/compiled-releases" \
     "node-exporter/$node_exporter_version" "ubuntu-trusty/$stemcell_version"
 
-gbe delete "$subsys_name" -n
+gbe delete -y "$subsys_name"
