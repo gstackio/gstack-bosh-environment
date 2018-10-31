7c0f8f95adb756eda14d6faac36f3e93af56598f
Benjamin Gandon <benjamin@gstack.io>
2018-10-31 03:20:46 +0100

    Stick to trusty Stemcell v3586.52 for Dingo PostgreSQL + Rewrite pre-deploy hooks

--- deployments/postgres/pre-deploy-once.sh
+++ deployments/postgres/pre-deploy-once.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 SUBSYS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 function spec_var() {
@@ -7,26 +9,62 @@ function spec_var() {
     bosh int "$SUBSYS_DIR/conf/spec.yml" --path "$path"
 }
 
-set -ex
+function create_upload_release_if_necessary_or_if_stale() {
+    local release_name=$1
 
-create_release=true
-dev_release_name=dingo-postgresql
-
-if [[ $create_release == true ]]; then
+    local rsc_name create_dev_release latest_dev_release \
+        latest_dev_release_commit_hash latest_git_commit_hash
     rsc_name=$(spec_var /input_resources/0/name)
     pushd "$BASE_DIR/.cache/resources/$rsc_name" || exit 115
-        git submodule update --init --recursive
+        latest_dev_release=$(
+            ls -t dev_releases/$release_name/$release_name-*.yml 2> /dev/null \
+                | head -n 1
+        )
 
-        latest_dev_release=$(ls -t dev_releases/$dev_release_name/$dev_release_name-*.yml 2> /dev/null | head -n 1)
-        if [[ -z $latest_dev_release \
-                || $(bosh int "$latest_dev_release" --path /commit_hash) != $(git rev-parse --short HEAD) ]]; then
-            bosh reset-release
-            bosh create-release
+        create_dev_release=false
+        if [[ -z $latest_dev_release ]]; then
+            create_dev_release=true
+        else
+            latest_dev_release_commit_hash=$(bosh int "$latest_dev_release" --path /commit_hash)
+            latest_git_commit_hash=$(git rev-parse --short HEAD)
+            if [[ $latest_dev_release_commit_hash != $latest_git_commit_hash ]]; then
+                create_dev_release=true
+            fi
         fi
-        # base_version=0.10.2
-        # if bosh inspect-release "$dev_release_name/${base_version}+dev.1" &> /dev/null; then
-        #     bosh delete-release "$dev_release_name/${base_version}+dev.1"
-        # fi
+
+        if [[ $create_dev_release == true ]]; then
+            git submodule update --init --recursive
+            bosh create-release --force
+        fi
+
         bosh upload-release
     popd
-fi
+}
+
+existing_releases=()
+function compute_existing_releases() {
+    local release_name=$1
+
+    if [[ -n $existing_releases ]]; then
+        return
+    fi
+    existing_releases=(
+        $(bosh releases --column name --column version \
+            | awk "/^${release_name}[[:blank:]]/"'{print $1 "/" $2}')
+    )
+}
+
+function create_upload_release_if_missing() {
+    local release_name=$1
+
+    compute_existing_releases "$release_name"
+    if [[ -n $existing_releases ]]; then
+        return
+    fi
+
+    create_upload_release_if_necessary_or_if_stale "$release_name"
+}
+
+set -x
+
+create_upload_release_if_missing "dingo-postgresql"
--- deployments/postgres/pre-deploy.sh
+++ deployments/postgres/pre-deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 SUBSYS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 function spec_var() {
@@ -7,26 +9,68 @@ function spec_var() {
     bosh int "$SUBSYS_DIR/conf/spec.yml" --path "$path"
 }
 
-set -ex
+function delete_any_existing_unused_release() {
+    local release_name=$1
+    compute_existing_releases "$release_name"
+    for existing_release in "${existing_releases[@]}"; do
+        # Don't delete release that are already used
+        if [[ $existing_release != *\* ]]; then
+            bosh delete-release -n "$existing_release"
+        fi
+    done
+}
 
-developing=false
-dev_release_name=dingo-postgresql
-base_version=0.10.2
+function create_upload_release_if_necessary_or_if_stale() {
+    local release_name=$1
 
-if [[ $developing == true ]]; then
+    local rsc_name create_dev_release latest_dev_release \
+        latest_dev_release_commit_hash latest_git_commit_hash
     rsc_name=$(spec_var /input_resources/0/name)
     pushd "$BASE_DIR/.cache/resources/$rsc_name" || exit 115
-        # git submodule update --init --recursive
+        latest_dev_release=$(
+            ls -t dev_releases/$release_name/$release_name-*.yml 2> /dev/null \
+                | head -n 1
+        )
 
-        latest_dev_release=$(ls -t dev_releases/$dev_release_name/$dev_release_name-*.yml 2> /dev/null | head -n 1)
-        if [[ -z $latest_dev_release \
-                || $(bosh int "$latest_dev_release" --path /commit_hash) != $(git rev-parse --short HEAD) ]]; then
-            # bosh reset-release
-            bosh create-release --force
-            # if bosh inspect-release "$dev_release_name/${base_version}+dev.1" &> /dev/null; then
-            #     bosh delete-release "$dev_release_name/${base_version}+dev.1"
-            # fi
-            bosh upload-release
+        create_dev_release=false
+        if [[ -z $latest_dev_release ]]; then
+            create_dev_release=true
+        else
+            latest_dev_release_commit_hash=$(bosh int "$latest_dev_release" --path /commit_hash)
+            latest_git_commit_hash=$(git rev-parse --short HEAD)
+            if [[ $latest_dev_release_commit_hash != $latest_git_commit_hash ]]; then
+                create_dev_release=true
+            fi
         fi
+
+        if [[ $create_dev_release == true ]]; then
+            git submodule update --init --recursive
+            bosh create-release --force
+        fi
+
+        delete_any_existing_unused_release "$release_name"
+
+        bosh upload-release
     popd
+}
+
+existing_releases=()
+function compute_existing_releases() {
+    local release_name=$1
+
+    if [[ -n $existing_releases ]]; then
+        return
+    fi
+    existing_releases=(
+        $(bosh releases --column name --column version \
+            | awk "/${release_name}/"'{print $1 "/" $2}')
+    )
+}
+
+set -x
+
+developing=false
+
+if [[ $developing == true ]]; then
+    create_upload_release_if_necessary_or_if_stale "dingo-postgresql"
 fi
