35fbc50d88200ee6fa8058b515e6f68c8995cb57
Benjamin Gandon <benjamin@gstack.io>
2018-11-07 18:49:12 +0100

    Bump Logsearch to v209.0.1

--- deployments/logsearch/post-deploy.sh
+++ deployments/logsearch/post-deploy.sh
@@ -1,5 +1,41 @@
 #!/usr/bin/env bash
 
-set -ex
+set -o pipefail
 
-bosh run-errand smoke-tests
+function run_errand_with_retry_for_debugging() {
+    local errand_name=$1
+    local errand_vm_name=$2
+
+    set -x
+    bosh run-errand "${errand_name}" --when-changed
+    error=$?
+    set +x -e
+
+    if [[ ${error} -eq 0 ]]; then
+        return 0
+    fi
+
+    # When there's an error, we restart the smoke tests, collecting logs and
+    # keeping the VM arround for debugging purpose.
+
+    logs_dir="${BASE_DIR}/logs/$(basename "${SUBSYS_DIR}")/${errand_name}"
+    mkdir -p "${logs_dir}"
+
+    set +e -x
+    time bosh run-errand "${errand_name}" --keep-alive --download-logs --logs-dir="${logs_dir}"
+    error=$?
+    set +x -e
+
+    if [[ ${error} -eq 0 ]]; then
+        # Whenever the second try succeeds, we delete the collected logs and
+        # any dedicated errand VM.
+        rm -r "${logs_dir}"
+        if [[ -n ${errand_vm_name} ]]; then
+            bosh --non-interactive stop --hard "${errand_vm_name}"
+        fi
+    fi
+    return ${error}
+}
+
+run_errand_with_retry_for_debugging "smoke-tests"
+exit $?
