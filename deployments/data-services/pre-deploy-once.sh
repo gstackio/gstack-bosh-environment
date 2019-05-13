3d39f5e5a294f840353905b163da74b0ffa4ef4e
Benjamin Gandon <benjamin@gstack.io>
2019-05-13 18:08:59 +0200

    Workaround issue with broker-registrar-boshrelease v3.5.0

--- deployments/data-services/pre-deploy-once.sh
+++ deployments/data-services/pre-deploy-once.sh
@@ -6,14 +6,11 @@ BASE_DIR=${BASE_DIR:-$(git rev-parse --show-toplevel)}
 SUBSYS_DIR=${SUBSYS_DIR:-$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}
 
 source "${BASE_DIR}/lib/hooks-api/common.inc.bash"
-source "${BASE_DIR}/lib/hooks-api/cloud-foundry.inc.bash"
 source "${BASE_DIR}/lib/hooks-api/bosh-releases.inc.bash"
 
 function main() {
-    cf_enforce_security_group "rabbitmq"
-
     # Workaround issue with broker-registrar-boshrelease v3.5.0
-    input_resource_index="3"
+    input_resource_index="1"
     release_name="broker-registrar"
     create_upload_dev_release_only_if_missing \
         "${input_resource_index}" "${release_name}"
