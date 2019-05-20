3f8d03ae04c6943ea84864d4ff10bd7c699426c9
Benjamin Gandon <benjamin@gstack.io>
2019-05-21 01:34:47 +0200

    Bump Traefik release to v1.6.0 + Enable Traefik smoke tests

---
+++ deployments/traefik/post-deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+BASE_DIR=${BASE_DIR:-$(git rev-parse --show-toplevel)}
+SUBSYS_DIR=${SUBSYS_DIR:-$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}
+
+source "${BASE_DIR}/lib/hooks-api/bosh-errands.inc.bash"
+
+function _config() {
+    errand_name="smoke-tests"
+    errand_vm_name="" # none, as the errand is co-localized on a persistent VM
+}
+
+function main() {
+    _config
+
+    run_errand_with_retry_for_debugging \
+        "${errand_name}" "${errand_vm_name}" # --when-changed
+}
+
+main "$@"
